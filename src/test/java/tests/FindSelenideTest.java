f57eeb4cb8a3cba422b637e04b8babffe8506870
This13th <This13th@yandex.ru>
2021-11-18 10:23:28 +0600

    update FindSelenideTest.java.java

--- src/test/java/tests/FindSelenideTest.java
+++ src/test/java/tests/FindSelenideTest.java
@@ -1,24 +1,38 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.*;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 
 public class FindSelenideTest {
 
     @Test
     void shouldFindSelenideOnGithub(){
         //открыть страницу github
-        Selenide.open("https://github.com/");
+        open("https://github.com/");
         //ввести в поле поиска selenide
         $("[name = q]").setValue("selenide").pressEnter();
         //нажать enter
         $$("ul.repo-list li").first().$("a").click();
         //выбрать первый найденый проект из списка
         //проверка: в заголовке встречается selenide/selenide
-        $("h1").shouldHave(Condition.text("selenide / selenide"));
+        $("h1").shouldHave(text("selenide / selenide"));
+    }
+
+    @Test
+    void firstContributorShouldBeSolntsev(){
+        //Configuration.headless = true; -запуск с бразером в фоне
+        //открыть страницу selenide
+        open("https://github.com/selenide/selenide");
+        //подвести мышку к первому элементу в области Contirbutors
+        $(".BorderGrid").$(byText("Contributors"))
+                .closest("div").$("li").hover();
+        //проверка: в появившемся окне(overlay)
+        $$(".Popover-message").findBy(visible)
+                .shouldHave(text("Andrei Solntsev"));
     }
 }
